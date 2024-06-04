683e2c1babc8d1c53aa615185bf786d579f1eee4
Nicolas Von Dolinger <nicolasvondolinger@gmail.com.br>
2024-06-04 14:45:52 -0300

    tp3 start

--- tp2/main.cpp
+++ tp2/main.cpp
@@ -112,54 +112,6 @@
      
     const int INF = 0x3f3f3f3f;
     const ll LINF = 0x3f3f3f3f3f3f3f3fll;
-     
-    void DBG() {
-        cerr << "]" << endl;
-    }
-     
-    void DBGC() {
-        cerr << "]" << endl;
-    }
-     
-    template<class H, class... T> void DBG(H h, T... t) {
-        cerr << to_string(h);
-        if(sizeof...(t)) cerr << ", ";
-        DBG(t...);
-    }
-     
-    template<class H, class... T> void DBGC(H h, T... t) {
-        for(auto& x: h) cerr << x << " ";
-        if(sizeof...(t)) cerr << "], [ ";
-        DBGC(t...);
-    }
-     
-    #ifndef _DEBUG
-    #define dbg(...) cerr << "[" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
-    #define dbgc(...) cerr << "["<< #__VA_ARGS__ << "]: [ "; DBGC(__VA_ARGS__) 
-    #else
-    #define dbg(...) 0
-    #define dbgc(...) 0
-    #endif
-     
-    bool isPrime(ll n){
-        for(int i = 2; i*i <= n; i++) if(n%i == 0) return false;
-        return true;
-    }
-
-    ll mdc(ll a, ll b){
-        if(a%b == 0) return b;
-        else return mdc(b, a%b);
-    }
-
-    ll mmc(ll a, ll b){
-        ll r, n, m; n = a, m = b;
-        do{
-            r = n % m;
-            n = m;
-            m = r;
-        } while(r != 0);
-        return (a*b)/ n;
-    }
 
     void print(vector<int>& cost){
         for(unsigned i = 0; i < cost.size(); i++){
@@ -167,17 +119,7 @@
         }
     }
 
-    int minDistance(vector<int>& ans, vector<bool>& visited){
-        int min_cost = INT_MAX, min_index;
-        rep(i, 0, ans.size()){
-            if(ans[i] <= min_cost && !visited[i]){
-                min_cost = ans[i], min_index = i;
-            }
-        }
-        return min_index;
-    }
-
-    vector<int> dijkstra(vector<vector<pair<int, pair<int, int>>>>& graph){
+    vector<int> dijkstra_years(vector<vector<pair<int, pair<int, pair<int, int>>>>>& graph){
         priority_queue<pair<int, int>> pq;
         vector<int> ans(graph.size(), INF);
 
@@ -189,7 +131,7 @@
             pq.pop();
             for(auto x: graph[u]){
                 int v = x.ff;
-                int w = x.ss.ff;
+                int w = x.ss.ss.ff;
                 if(ans[v] > ans[u] + w){
                     ans[v] = ans[u] + w;
                     pq.push(make_pair(-ans[v], v));
@@ -200,23 +142,77 @@
         return ans;
     }
 
-    void solve(){
-        int n, m; cin >> n >> m;
-        vector<vector<pair<int, pair<int, int>>>> graph(n); 
-        pair<int, pair<int, int>> info; 
-        int b = -1;
-        rep(i, 0, m){
-            int u, v, a, l, c; cin >> u >> v >> a >> l >> c; 
-            u--, v--; b = max(b, a);
-            info.ff = v; info.ss.ff = l, info.ss.ss = c; 
-            graph[u].pb(info);
-            info.ff = u; graph[v].pb(info);
+    ll dijkstra_cost(vector<vector<pair<int, pair<int, pair<int, int>>>>>& graph){
+        ll t = 0;
+        priority_queue<pair<int, int>> pq;
+        vector<int> ans(graph.size(), INF);
+
+        pq.push(make_pair(-0, 0));
+        ans[0] = 0;
+
+        while(!pq.empty()){
+            int u = pq.top().ss;
+            pq.pop();
+            for(auto x: graph[u]){
+                int v = x.ff;
+                int w = x.ss.ss.ss;
+                if(ans[v] > ans[u] + w){
+                    t += w;
+                    ans[v] = ans[u] + w;
+                    pq.push(make_pair(-ans[v], v));
+                }
+            }
         }
 
-        vector<int> cost = dijkstra(graph);
+        //print(ans);
+        return t;
+    }
 
-        print(cost);
-        cout << b << endl;
+    void dfs(vector<vector<pair<int, pair<int, pair<int, int>>>>>& graph, vector<bool>& visited, int n, int max){
+        visited[n] = true;
+
+        for(auto w: graph[n])if(!visited[w.ff] && w.ss.ff <= max){
+            dfs(graph, visited,  w.ff, max);
+        }
+    }
+
+    void solve(){
+        int n, m; cin >> n >> m;
+        vector<vector<pair<int, pair<int, pair<int, int>>>>> graph(n); 
+        pair<int, pair<int, pair<int, int>>> p1, p2;
+        set<int> years;
+        int u, v, a, l, c, n1 = -1;
+        rep(i, 0, m){
+            cin >> u >> v >> a >> l >> c; u--, v--; 
+            n1 = max(n1, a);
+            years.insert(a);
+            p1.ff = v; p1.ss.ff = a, p1.ss.ss.ff = l, p1.ss.ss.ss = c; 
+            graph[u].pb(p1);
+            p2.ff = u; p2.ss.ff = a, p2.ss.ss.ff = l, p2.ss.ss.ss = c;
+            graph[v].pb(p2);
+        }
+
+        vector<int> cost = dijkstra_years(graph);
+        int n2;
+        for(auto i: years){
+            int count = 0;
+            vector<bool> visited(n, false);
+            for(int j = 0; j < n; j++)if(!visited[j]){
+                if(count == 2) break;
+                dfs(graph, visited, j, i);
+                count++;
+            }
+            if(count == 1){
+                n2 = i;
+                break;
+            }
+        }
+
+        print(cost); // Horas
+        cout << n1 << endl; // Distâncias Mutuamente Realizáveis 
+        cout << n2 << endl; // Ano a partir do qual o grafo é conexo
+        cout << dijkstra_cost(graph) << endl; // Menor custo para conectar o reino
+
     }
      
     int main(){ 
